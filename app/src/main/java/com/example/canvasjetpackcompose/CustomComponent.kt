2262c3f32318e5ab991f1b85a0510202f0063d56
navneetpal01 <navneetpalgithub@gmail.com>
2024-02-07 23:17:12 +0530

    Fourth

--- app/src/main/java/com/example/canvasjetpackcompose/CustomComponent.kt
+++ app/src/main/java/com/example/canvasjetpackcompose/CustomComponent.kt
@@ -1,12 +1,21 @@
 package com.example.canvasjetpackcompose
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
@@ -14,17 +23,37 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.DefaultAlpha
 
 @Composable
 fun CustomComponent(
-    canvasSize : Dp = 300.dp,
+    canvasSize: Dp = 300.dp,
     indicatorValue: Int = 0,
-    maxIndicatorValue : Int = 100,
-    backgroundIndicatorColor : Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-    backgroundIndicatorStrokeWidth : Float = 100f
-){
+    maxIndicatorValue: Int = 1000,
+    backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+    backgroundIndicatorStrokeWidth: Float = 100f,
+    foregroundIndicatorColor: Color = MaterialTheme.colorScheme.primary,
+    foregroundIndicatorWidth: Float = 100f
+) {
+    var allowedIndicatorValue by remember{
+        mutableStateOf(maxIndicatorValue)
+    }
+    allowedIndicatorValue = if(indicatorValue <= maxIndicatorValue){
+        indicatorValue
+    }else{
+        maxIndicatorValue
+    }
+    val animatedIndicatorValue = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(allowedIndicatorValue) {
+        animatedIndicatorValue.animateTo(allowedIndicatorValue.toFloat())
+    }
+    val percentage = (animatedIndicatorValue.value / maxIndicatorValue) * 100
+    val sweepAngle by animateFloatAsState(
+        targetValue = (2.4 * percentage).toFloat(),
+        animationSpec = tween(1000)
+    )
+
     Column(
         modifier = Modifier
             .size(canvasSize)
@@ -35,17 +64,23 @@ fun CustomComponent(
                     indicatorColor = backgroundIndicatorColor,
                     indicatorStrokeWidth = backgroundIndicatorStrokeWidth
                 )
+                foregroundIndicator(
+                    sweepAngle = sweepAngle,
+                    componentSize = componentSize,
+                    indicatorColor = foregroundIndicatorColor,
+                    indicatorStrokeWidth = foregroundIndicatorWidth
+                )
             }
-    ){
+    ) {
 
     }
 }
 
 fun DrawScope.backgroundIndicator(
-    componentSize : Size,
-    indicatorColor : Color,
-    indicatorStrokeWidth : Float
-){
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
     drawArc(
         size = componentSize,
         color = indicatorColor,
@@ -58,7 +93,31 @@ fun DrawScope.backgroundIndicator(
         ),
         topLeft = Offset(
             x = (size.width - componentSize.width) / 2f,
-            y = (size.height - componentSize.height) /2f,
+            y = (size.height - componentSize.height) / 2f,
+        )
+
+    )
+}
+
+fun DrawScope.foregroundIndicator(
+    sweepAngle: Float,
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = sweepAngle,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2f,
+            y = (size.height - componentSize.height) / 2f,
         )
 
     )
@@ -66,6 +125,6 @@ fun DrawScope.backgroundIndicator(
 
 @Preview(showBackground = true)
 @Composable
-fun CustomComponentPreview(){
-   CustomComponent()
+fun CustomComponentPreview() {
+    CustomComponent()
 }
--- app/src/main/java/com/example/canvasjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/canvasjetpackcompose/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.canvasjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.canvasjetpackcompose.ui.theme.CanvasJetpackComposeTheme
 
@@ -17,7 +26,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CanvasJetpackComposeTheme {
-
+              Column(
+                  modifier = Modifier.fillMaxSize(),
+                  horizontalAlignment = Alignment.CenterHorizontally
+              ){
+                  var value by remember{
+                      mutableStateOf(300)
+                  }
+                  CustomComponent(
+                      indicatorValue = value
+                  )
+                  TextField(
+                      value = value.toString(),
+                      onValueChange = {
+                          value = if (it.isNotEmpty()){
+                              it.toInt()
+                          } else{
+                              0
+                          }
+                      },
+                      keyboardOptions = KeyboardOptions(
+                          keyboardType = KeyboardType.Number
+                      )
+                  )
+              }
             }
         }
     }
